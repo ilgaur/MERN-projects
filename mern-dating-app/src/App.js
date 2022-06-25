43bb3bb9e18f9121574f3bd272601f63de37c6ee
ilqar <pvtnumber50@protonmail.com>
2022-06-25 14:47:57 +0430

    adding swipe button, some compatibility issues with MUI library, WILL FIX EM LATER 👋

--- mern-dating-app/src/App.js
+++ mern-dating-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header.js';
 import DatingCard from './components/DatingCard';
+import SwipeButton from './components/SwipeButton'
 function App() {
 return (
 <div className="app">
 <Header />
 <DatingCard />
+<SwipeButton />
 </div>
 );
 }
