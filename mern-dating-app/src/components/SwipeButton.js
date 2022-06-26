b6bdd8bf438aabab83c32cfe653da43a4ac055c9
ilqar <pvtnumber50@protonmail.com>
2022-06-26 14:27:33 +0430

    OKAYED button + added CSS + installed express + mongoose, trying to reslove the failed to parse JSON data 🪲

--- mern-dating-app/src/components/SwipeButton.js
+++ mern-dating-app/src/components/SwipeButton.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './SwipeButtons.css'
-import ReplayIcon from '@mui/icons-material/Replay'
-import CloseIcon from '@mui/icons-material/Close'
-import StarRateIcon from '@mui/icons-material/StarRate'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import FlashOnIcon from '@mui/icons-material/FlashOn''
-import IconButton from '@mui/icons-material/IconButton'
+import './SwipeButton.css'
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import IconButton from '@mui/material/IconButton';
 
 const SwipeButtons = () => {
     return (
